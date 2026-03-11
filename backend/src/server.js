96c1186c12e1ad092f386ef2e1935c92a75d6c56
Ganta-Ranganayakulu <ganta.ranganayakulu.06@gmail.com>
2026-03-11 23:43:05 +0530

    fix :bind server

--- backend/src/server.js
+++ backend/src/server.js
@@ -51,4 +51,4 @@ app.use('/api/profile', require('./routes/profile'));
 app.get('/', (req, res) => res.json({ message: 'SkillSync API Running' }));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
