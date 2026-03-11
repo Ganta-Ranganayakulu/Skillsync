7d70bd1027dfb95b395a058fca5a1bcf8fafcfbf
Ganta-Ranganayakulu <ganta.ranganayakulu.06@gmail.com>
2026-03-11 22:37:00 +0530

    fix : Configure CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,16 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+   origin: [
+    'https://skillsync-frontend-psi.vercel.app',
+    'http://localhost:3000'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
